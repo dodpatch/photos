1a11e8fbbe53cf65911fad96d1a011db71aa3c1c
optimus <paulido92@gmail.com>
2020-01-29 17:55:39 +0000

    helpers created

---
+++ app/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists('currentRoute')) {
+
+    function currentRoute(...$routes) {
+        foreach ($routes as $route) {
+            if (request()->url() == $route) {
+                return ' active';
+            }
+        }
+    }
+
+}
